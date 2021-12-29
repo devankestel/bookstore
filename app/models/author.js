fbe548faca67d28ea0792e7a45f9680e8a3af8c5
Devan Kestel <devan.kestel@gmail.com>
2021-12-29 00:44:20 -0500

    Polymorphic model and fix dependency hell.

--- app/models/author.js
+++ app/models/author.js
@@ -1,6 +1,7 @@
 import DS from 'ember-data';
+import Publisher from './publisher';
 
-export default DS.Model.extend({
+export default Publisher.extend({
   name: DS.attr('string'),
   books: DS.hasMany('book')
 });
